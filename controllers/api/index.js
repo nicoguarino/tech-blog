8be364a63f589b1b3169495a08ea04ab36691bbc
Nico (Filipu) Guarino <nizoworld@gmail.com>
2021-10-30 16:53:56 -0700

    finished index.js in api folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,2 +1,13 @@
 //router use /user for routes user-routes
     //all routes in user-routes will start with /api/user/
+const router = require('express').Router();
+
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+
+module.exports = router;
