c2a3892a04e6ea8e9c64c4d66ccfe381d3f59e58
Nico (Filipu) Guarino <nizoworld@gmail.com>
2021-10-30 16:19:57 -0700

    finished index.js in model

---
+++ models/index.js
@@ -0,0 +1,21 @@
+const Post = require('./Post');
+const User = require('./User');
+const Comment = require('./Comment');
+
+User.hasMany(Post);
+
+Post.belongsTo(User, {
+    onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(User, {
+    onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+    onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment);
+
+module.exports = { User, Post, Comment }
