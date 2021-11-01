850d4ee3a5d1004bee83fffedaf8fcc61c93b0e5
Nico (Filipu) Guarino <nizoworld@gmail.com>
2021-11-01 11:23:57 -0700

    fixed back end connection errors

--- controllers/public/home-routes.js
+++ controllers/public/home-routes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Post, User, Comment} = require('../models');
+const { Post, User, Comment} = require('../../models');
 
 // get all posts for homepage
 router.get('/', (req, res) => {
--- controllers/public/index.js
+++ controllers/public/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes');
 
-router.use('/home', homeRoutes);
+router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
